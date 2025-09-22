70fc0377ead4d0c4ebb3e736b4f61363a939d769
Filip <filip_yousif@hotmail.com>
2025-09-22 08:41:22 +0200

    .

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,6 +3,7 @@ import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -13,7 +14,10 @@ const app = express();
 
 
 //Middleware
-
+app.use(cors({
+    origin:"http://localhost:5173",
+})
+);
 app.use(express.json());
 app.use(rateLimiter);
 
